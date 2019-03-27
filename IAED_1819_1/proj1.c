b5eb2c3cbddc0559ea2da8dbab5d084e974ed24f
Rafael-R <rsrodrigues.99@gmail.com>
2019-03-27 14:01:56 +0000

    Bug fixing.

--- IAED_1819_1/proj1.c
+++ IAED_1819_1/proj1.c
@@ -8,17 +8,28 @@
 
 
 int main() {
-    char info[MAX_INFO], command;
+    char info[MAX_INFO], command, splited_info[MAX_PARAM][MAX_CHAR];
+    int num_parameters;
     Event parameters;
 
     while (command != 'x') {
 
         fgets(info, MAX_INFO, stdin);
         command = info[0];
-        parameters = processInfo(info, command);
+        num_parameters = splitInfo(info, splited_info);
 
         switch (command) {
             case 'a':
+                strcpy(parameters.description, splited_info[0]);
+                parameters.date = stringToInt(splited_info[1]);
+                parameters.start = stringToInt(splited_info[2]);
+                parameters.duration = stringToInt(splited_info[3]);
+                parameters.room = stringToInt(splited_info[4]);
+                strcpy(parameters.responsible, splited_info[5]);
+                strcpy(parameters.attendants[0], splited_info[6]);
+                strcpy(parameters.attendants[1], splited_info[7]);
+                strcpy(parameters.attendants[2], splited_info[8]);
+                parameters.num_attendants = num_parameters - 6;
                 __a__(parameters);
                 break;
 
@@ -27,31 +38,31 @@ int main() {
                 break;
 
             case 's':
-                __s__(parameters.room);
+                __s__(stringToInt(splited_info[0]));
                 break;
 
             case 'r':
-                __r__(parameters.description);
+                __r__(splited_info[0]);
                 break;
 
             case 'i':
-                __i__(parameters.description, parameters.start);
+                __i__(splited_info[0], stringToInt(splited_info[1]));
                 break;
 
             case 't':
-                __t__(parameters.description, parameters.duration);
+                __t__(splited_info[0], stringToInt(splited_info[1]));
                 break;
 
             case 'm':
-                __m__(parameters.description, parameters.room);
+                __m__(splited_info[0], stringToInt(splited_info[1]));
                 break;
 
             case 'A':
-                __A__(parameters.description, parameters.attendants[0]);
+                __A__(splited_info[0], splited_info[1]);
                 break;
 
             case 'R':
-                __R__(parameters.description, parameters.attendants[0]);
+                __R__(splited_info[0], splited_info[1]);
                 break;
         }
     }
