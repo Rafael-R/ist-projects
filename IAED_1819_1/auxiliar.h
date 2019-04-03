c773c5f627aab83feb2793afa39c4a58f9bb2483
Rafael <rsrodrigues.99@gmail.com>
2019-04-03 21:40:40 +0100

    Mooshak 16. TODO: Beautify and comments.

--- IAED_1819_1/auxiliar.h
+++ IAED_1819_1/auxiliar.h
@@ -1,6 +1,6 @@
 /*
  * File:  auxiliar.h
- * Author:  Rafael Rodrigues
+ * Author:  Rafael Rodrigues 89532
  * Description: A file that contains all the commands needed for
                 the execution of the project.
 */
@@ -10,11 +10,17 @@
 #include <string.h>
 
 
-#define MAX_INPUT 344
-#define MAX_PARAMETERS 9
-#define MAX_CHAR 64
+/* Numero maximo de eventos */
 #define MAX_EVENTS 1000
+/* Numero maximo de caracteres do input */
+#define MAX_INPUT 344
+/* Numero maximo de informacoes de um comando */
+#define MAX_INFO 9
+/* Numero maximo de caracteres da descricao e dos nomes */
+#define MAX_CHAR 64
+/* Numero maximo de participantes */
 #define MAX_ATTENDANTS 3
+
 #define UNDEFINED 1001
 #define FALSE 0
 #define TRUE 1
@@ -27,44 +33,9 @@ typedef struct {
     int duration;
     int room;
     char responsible[MAX_CHAR];
-    char attendants[MAX_ATTENDANTS][MAX_CHAR];
     int num_attendants;
+    char attendants[MAX_ATTENDANTS][MAX_CHAR];
 } Event;
 
 
-Event events[MAX_EVENTS];
-
-int num_events;
-
-
-/* ------------------- DECLARACAO DAS FUNCOES AUXILIARES -------------------- */
-
-
-int calcEnd(int start, int duration);
-
-
-int verifyTime(Event event, Event test);
-
-
-int verifyRoom(Event event, Event test);
-
-
-int getAttendantIndex(Event event, char attendant[]);
-
-
-int verifyAttendant(Event event, char attendant[]);
-
-
-int verifications(int index, Event test);
-
-
-int getEventIndex(char description[]);
-
-
-long sortData(Event event);
-
-
-void sortEvents();
-
-
-void printEvent(Event event);
+/* -------------------------------------------------------------------------- */
