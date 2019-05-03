2e71a82924670979329d729b34dcc6216074f816
Rafael <rsrodrigues.99@gmail.com>
2019-05-03 22:30:54 +0100

    Folders organized.

---
+++ IAED_1819/p2/proj2.c
@@ -0,0 +1,9 @@
+/*
+ * File:  proj2.c
+ * Author:  Rafael Rodrigues 89532
+ * Description: Segundo Projeto da cadeira de IAED
+*/
+
+#include "auxiliar.h"
+
+/* -------------------------------------------------------------------------- */
