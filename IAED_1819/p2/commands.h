ca2cd9ebb123d727194174c6fa76b79911676da3
Rafael <rsrodrigues.99@gmail.com>
2019-05-07 11:28:37 +0100

    Little fixes. TODO: Fix test __r__().

---
+++ IAED_1819/p2/commands.h
@@ -0,0 +1,20 @@
+/*
+ * File:  proj2.c
+ * Author:  Rafael Rodrigues 89532
+ * Description: Segundo Projeto da cadeira de IAED
+*/
+
+#include "hashtable.h"
+
+
+link __a__(link *heads, char *name, char *local, char *domain, char *phone);
+
+void __l__(link *heads, int size);
+
+void __p__(link *heads, char *name);
+
+void __r__(link *heads, char *name);
+
+void __e__(link *heads, char *name, char *new_local, char *new_domain);
+
+void __c__(link* heads, char *domain);
