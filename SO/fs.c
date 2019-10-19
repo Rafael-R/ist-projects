e8a984024098fc45c10d979e517e5f9a15662138
Rafael-R <rsrodrigues.99@gmail.com>
2019-10-19 18:13:13 +0100

    4 arguments input and hash table implemented.

--- SO/fs.c
+++ SO/fs.c
@@ -10,35 +10,46 @@ int obtainNewInumber(tecnicofs* fs) {
 }
 
 tecnicofs* new_tecnicofs(int numberBuckets){
-	tecnicofs*fs = malloc(sizeof(tecnicofs));
+	tecnicofs* fs = malloc(sizeof(tecnicofs));
 	if (!fs) {
 		perror("failed to allocate tecnicofs");
 		exit(EXIT_FAILURE);
 	}
-	fs->bstRoot = NULL;
+	fs->bstRoots = malloc(sizeof(node) * numberBuckets);
+	fs->buckets = numberBuckets;
+	for (size_t i = 0; i < fs->buckets; i++) 	{
+		fs->bstRoots[i] = NULL;
+	}
 	fs->nextINumber = 0;
  	return fs;
- }
+}
 
 void free_tecnicofs(tecnicofs* fs){
-	free_tree(fs->bstRoot);
+	for (size_t i = 0; i < fs->buckets; i++) 	{
+		free_tree(fs->bstRoots[i]);
+	}
 	free(fs);
 }
 
 void create(tecnicofs* fs, char *name, int inumber){
-	fs->bstRoot = insert(fs->bstRoot, name, inumber);
+	int index = hash(name, fs->buckets);
+	fs->bstRoots[index] = insert(fs->bstRoots[index], name, inumber);
 }
 
 void delete(tecnicofs* fs, char *name){
-	fs->bstRoot = remove_item(fs->bstRoot, name);
+	int index = hash(name, fs->buckets);
+	fs->bstRoots[index] = remove_item(fs->bstRoots[index], name);
 }
 
 int lookup(tecnicofs* fs, char *name){
-	node* searchNode = search(fs->bstRoot, name);
+	int index = hash(name, fs->buckets);
+	node* searchNode = search(fs->bstRoots[index], name);
 	if ( searchNode ) return searchNode->inumber;
 	return 0;
 }
 
 void print_tecnicofs_tree(FILE * fp, tecnicofs *fs){
-	print_tree(fp, fs->bstRoot);
+	for (size_t i = 0; i < fs->buckets; i++) 	{
+		print_tree(fp, fs->bstRoots[i]);
+	}
 }
