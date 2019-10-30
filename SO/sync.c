134b6e2dfe1e0f48c905923c080f5962d0a86b2a
Rafael-R <rsrodrigues.99@gmail.com>
2019-10-30 14:53:17 +0000

    Timer redo inspired in solution

--- SO/sync.c
+++ SO/sync.c
@@ -3,49 +3,56 @@
 #include <stdlib.h>
 
 void sync_init(LOCK_TYPE* sync){
-    if(LOCK_INIT(sync) != 0){
+    int ret = LOCK_INIT(sync);
+    if(ret != 0){
         fprintf(stderr, "Error: initializing lock\n");
         exit(EXIT_FAILURE);
     }
 }
 
 void sync_destroy(LOCK_TYPE* sync){
-    if(LOCK_DESTROY(sync) != 0){
-        fprintf(stderr, "Error: initializing lock\n");
+    int ret = LOCK_DESTROY(sync);
+    if(ret != 0){
+        fprintf(stderr, "Error: destroying lock\n");
         exit(EXIT_FAILURE);
     }
 }
 
 void sync_wrlock(LOCK_TYPE* sync){
-    if(LOCK_WRLOCK(sync) != 0){
+    int ret = LOCK_WRLOCK(sync);
+    if(ret != 0){
         fprintf(stderr, "Error: write locking lock\n");
         exit(EXIT_FAILURE);
     }
 }
 
 void sync_rdlock(LOCK_TYPE* sync){
-    if(LOCK_RDLOCK(sync) != 0){
+    int ret = LOCK_RDLOCK(sync);
+    if(ret != 0){
         fprintf(stderr, "Error: read locking lock\n");
         exit(EXIT_FAILURE);
     }
 }
 
 void sync_unlock(LOCK_TYPE* sync){
-    if(LOCK_UNLOCK(sync) != 0){
+    int ret = LOCK_UNLOCK(sync);
+    if(ret != 0){
         fprintf(stderr, "Error: unlocking lock\n");
         exit(EXIT_FAILURE);
     }
 }
 
 void thread_create(pthread_t *thread, void *(*function) (void *)) {
-    if((pthread_create(thread, NULL, function, NULL)) != 0) {
+    int ret = pthread_create(thread, NULL, function, NULL);
+    if(ret != 0){
         fprintf(stderr, "Error: creating thread\n");
         exit(EXIT_FAILURE);
     }
 }
 
 void thread_join(pthread_t thread) {
-    if((pthread_join(thread, NULL)) != 0) {
+    int ret = pthread_join(thread, NULL);
+    if(ret != 0){
         fprintf(stderr, "Error: joining thread\n");
         exit(EXIT_FAILURE);
     }
@@ -53,7 +60,8 @@ void thread_join(pthread_t thread) {
 
 void mutex_init(pthread_mutex_t* mutex){
     #if defined(MUTEX) || defined(RWLOCK)
-        if(pthread_mutex_init(mutex, NULL) != 0){
+        int ret = pthread_mutex_init(mutex, NULL);
+        if(ret != 0){
             fprintf(stderr, "Error: initializing mutex\n");
             exit(EXIT_FAILURE);
         }
@@ -62,7 +70,8 @@ void mutex_init(pthread_mutex_t* mutex){
 
 void mutex_destroy(pthread_mutex_t* mutex){
     #if defined(MUTEX) || defined(RWLOCK)
-        if(pthread_mutex_destroy(mutex) != 0){
+        int ret = pthread_mutex_destroy(mutex);
+        if(ret != 0){
             fprintf(stderr, "Error: destroying mutex\n");
             exit(EXIT_FAILURE);
         }
@@ -71,7 +80,8 @@ void mutex_destroy(pthread_mutex_t* mutex){
 
 void mutex_lock(pthread_mutex_t* mutex){
     #if defined(MUTEX) || defined(RWLOCK)
-        if(pthread_mutex_lock(mutex) != 0){
+        int ret = pthread_mutex_lock(mutex);
+        if(ret != 0){
             fprintf(stderr, "Error: locking mutex\n");
             exit(EXIT_FAILURE);
         }
@@ -80,7 +90,8 @@ void mutex_lock(pthread_mutex_t* mutex){
 
 void mutex_unlock(pthread_mutex_t* mutex){
     #if defined(MUTEX) || defined(RWLOCK)
-        if(pthread_mutex_unlock(mutex) != 0){
+        int ret = pthread_mutex_unlock(mutex);
+        if(ret != 0){
             fprintf(stderr, "Error: unlocking mutex\n");
             exit(EXIT_FAILURE);
         }
@@ -88,6 +99,6 @@ void mutex_unlock(pthread_mutex_t* mutex){
 }
 
 int nop(void* pointer){
-    (void) pointer;
+    (void)pointer;
     return 0;
 }
