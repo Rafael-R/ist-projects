818f3b4d83a3cf8b289c30789c193964118d7bce
Rafael-R <rsrodrigues.99@gmail.com>
2019-11-13 16:25:05 +0000

    Project restructure

--- SO/client/tecnicofs-client-api.h
+++ SO/client/tecnicofs-client-api.h
@@ -1,9 +1,11 @@
 #ifndef TECNICOFS_CLIENT_API_H
 #define TECNICOFS_CLIENT_API_H
 
-#include "tecnicofs-api-constants.h"
+#include "../tecnicofs-api-constants.h"
 
 
+int tfsMount(char * address);
+int tfsUnmount();
 int tfsCreate(char *filename, permission ownerPermissions, permission othersPermissions);
 int tfsDelete(char *filename);
 int tfsRename(char *filenameOld, char *filenameNew);
@@ -11,7 +13,5 @@ int tfsOpen(char *filename, permission mode);
 int tfsClose(int fd);
 int tfsRead(int fd, char *buffer, int len);
 int tfsWrite(int fd, char *buffer, int len);
-int tfsMount(char * address);
-int tfsUnmount();
 
 #endif /* TECNICOFS_CLIENT_API_H */
