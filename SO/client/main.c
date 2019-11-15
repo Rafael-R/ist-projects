208581b7d3ccf28eba03ed77e91afb3fb71c4a7a
Rafael-R <rsrodrigues.99@gmail.com>
2019-11-15 01:19:55 +0000

    SO: Server and one client comunicate, program compiles and makefiles work

--- SO/client/main.c
+++ SO/client/main.c
@@ -1,31 +1,39 @@
 #include "../tecnicofs-api-common.h"
 
 
+
+char* socketName;
+
+static void displayUsage (const char* appName);
+static void parseArgs (long argc, char* const argv[]);
+void* processInput();
+
+
 int main(int argc, char* argv[]) {
+
     int client_socket, status;
     struct sockaddr_un server_addr;
-    char name[] = "skrskr", socketName[] = "/tmp/";
-    char command[MAX_INPUT_SIZE];
+    char sendline[MAX_INPUT_SIZE];
+
+    parseArgs(argc, argv);
 
     // Cria socket stream
     client_socket = socket(AF_UNIX, SOCK_STREAM, 0);
-    check_status(client_socket, "client: can't open stream socket");
+    check_status(client_socket, "client: can't open stream socket\n");
 
     bzero((char *)&server_addr, sizeof(server_addr));
     server_addr.sun_family = AF_UNIX;
-    strcat(socketName, name);
     strcpy(server_addr.sun_path, socketName);
 
     status = connect(client_socket, (struct sockaddr *) &server_addr, sizeof(server_addr));
-    check_status(status, "client: can't connect to server");
+    check_status(status, "client: can't connect to server\n");
 
+    
     while (1) {
-        // Lê o input
-        printf("Enter command: ");
-        fgets(command, sizeof(command)/sizeof(char), stdin);
-
-        status = send(client_socket, command, sizeof(command), 0);
-        check_status(status, "client: can't send message");
+        printf("Enter command: "); 
+        fgets(sendline, MAX_INPUT_SIZE, stdin);
+        status = send(client_socket, sendline, strlen(sendline), 0);
+        check_status(status, "client: sending message\n");
     }
 
     /* Fecha o socket e termina */
@@ -33,3 +41,68 @@ int main(int argc, char* argv[]) {
 
     return 0;
 }
+
+
+
+static void displayUsage (const char* appName) {
+    printf("Usage: %s\n", appName);
+    exit(EXIT_FAILURE);
+}
+
+static void parseArgs (long argc, char* const argv[]) {
+    if (argc != 2) {
+        fprintf(stderr, "Invalid format:\n");
+        displayUsage(argv[0]);
+    }
+    socketName = argv[1];
+}
+
+/*
+void* processInput() {
+    FILE* input = openFile(inputFile, "r");
+    char line[MAX_INPUT_SIZE];
+    int lineNumber = 0;
+
+    while(fgets(line, sizeof(line)/sizeof(char), input)) {
+        lineNumber++;
+
+        char token;
+        char name[MAX_INPUT_SIZE], newName[MAX_INPUT_SIZE];
+
+        int numTokens = sscanf(line, "%c %s %s", &token, name, newName);
+
+        // perform minimal validation
+        if (numTokens < 1) {
+            continue;
+        }
+        switch (token) {
+            case 'c':
+            case 'l':
+            case 'd':
+                if(numTokens != 2) {
+                    errorParse(lineNumber);
+                } else {
+                    insertCommand(line);
+                    break;
+                }
+            case 'r':
+                if(numTokens != 3) {
+                    errorParse(lineNumber);
+                } else {
+                    insertCommand(line);
+                    break;
+                }
+            case '#':
+                break;
+            default: { // error
+                errorParse(lineNumber);
+            }
+        }
+    }
+    fclose(input);
+    for (int i = 0; i < numberThreads; i++) {
+        insertCommand(EXIT_COMMAND);
+    }
+    pthread_exit(NULL);
+}
+*/
