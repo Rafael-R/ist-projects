8d953c1aee67d65a729ec53aa0531417ea5583d2
Rafael-R <rsrodrigues.99@gmail.com>
2019-11-20 23:24:46 +0000

    PO: Small fixes

--- PO/project/m19-core/src/m19/users/UserComparator.java
+++ PO/project/m19-core/src/m19/users/UserComparator.java
@@ -4,6 +4,7 @@ import java.util.Comparator;
 
 public class UserComparator implements Comparator<User> {
 
+    @Override
     public int compare(User user1, User user2) {
         return (user1.compareTo(user2));
     }
