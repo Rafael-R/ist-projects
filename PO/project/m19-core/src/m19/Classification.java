d77818c7901487a3b606075addf71a70087b14a0
Rafael-R <rsrodrigues.99@gmail.com>
2019-12-07 14:58:23 +0000

    Removed packages, Passes all tests

--- PO/project/m19-core/src/m19/Classification.java
+++ PO/project/m19-core/src/m19/Classification.java
@@ -3,9 +3,6 @@ package m19;
 import java.io.Serializable;
 import java.util.List;
 
-import m19.Work;
-import m19.Request;
-
 public abstract class Classification implements Serializable {
 
     private static final long serialVersionUID = 201911281653L;
