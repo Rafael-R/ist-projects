f3027178e7ce5631c62582aec2e0dbbe4ba42324
Rafael-R <rsrodrigues.99@gmail.com>
2020-05-25 06:08:27 +0100

    Passes all tests

---
+++ IAED/1920/p2/auxiliar.h
@@ -0,0 +1,16 @@
+/* File:  auxiliar.h
+ * Author:  Andre Rodrigues 92421
+ * Description:
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define MAX_INPUT 1024
+
+char *copyString(char *original);
+
+int myCompare(const void *a, const void *b);
+
+void sort(char **arr, int n);
---
+++ IAED/1920/p2/team.c
@@ -0,0 +1,27 @@
+/* File:  team.c
+ * Author:  Andre Rodrigues 92421
+ * Description:
+ */
+
+#include "team.h"
+
+Team newTeam(char *name)
+{
+    Team team = (Team)malloc(sizeof(struct team));
+
+    team->name = copyString(name);
+    team->victories = 0;
+
+    return team;
+}
+
+void printTeam(Team team)
+{
+    printf("%s %d\n", team->name, team->victories);
+}
+
+void freeTeam(Team team)
+{
+    free(team->name);
+    free(team);
+}
