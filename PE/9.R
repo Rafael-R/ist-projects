a56bb5ab53be55fb80e4a5bcf6e2a1f74e9758a4
Rafael-R <rsrodrigues.99@gmail.com>
2024-06-05 20:15:24 +0100

    correto

--- PE/9.R
+++ PE/9.R
@@ -7,12 +7,27 @@ k <- 2.623
 
 set.seed(seed)
 
-amostras_H0 <- matrix(rpois(n * m, lambda_H0), nrow = m, ncol = n)
-amostras_H1 <- matrix(rpois(n * m, lambda_H1), nrow = m, ncol = n)
+erro_1_especie <- 0
+erro_2_especie <- 0
 
-erro_tipo_1 <- mean(rowMeans(amostras_H0) > k)  # erro de 1ª espécie
-erro_tipo_2 <- mean(rowMeans(amostras_H1) <= k)  # erro de 2ª espécie
+for (i in 1:m) {
+  amostra_H0 <- rpois(n, lambda_H0)
+  media_H0 <- mean(amostra_H0)
+  
+  if (media_H0 > k) {
+    erro_1_especie <- erro_1_especie + 1
+  }
+  
+  amostra_H1 <- rpois(n, lambda_H1)
+  media_H1 <- mean(amostra_H1)
+  
+  if (media_H1 <= k) {
+    erro_2_especie <- erro_2_especie + 1
+  }
+}
 
-quociente <- round(erro_tipo_2 / erro_tipo_1, digits = 2)
+prob_erro_1_especie <- erro_1_especie / m
+prob_erro_2_especie <- erro_2_especie / m
 
-cat(quociente, "\n")
+
+round(prob_erro_2_especie / prob_erro_1_especie, 2)
