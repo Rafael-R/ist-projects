a94b755dff276d946d93db62e50c1d68c4de87cf
Rafael-R <rsrodrigues.99@gmail.com>
2024-06-05 17:08:07 +0100

    0.3973

--- PE/7.R
+++ PE/7.R
@@ -3,12 +3,12 @@ library(stats4)
 # Dados da amostra
 amostra <- c(8.54, 4.76, 5.15, 4.96, 6.25, 7.22, 12.9, 6.04, 8.86, 4.88, 6.54,
              4.53, 4.7, 5.38, 5.96, 5.17, 5.09, 5.11)
+a <- 4.5
 
 # Função de log-verossimilhança
 log_verossimilhanca <- function(theta) {
   if (theta <= 0) return(Inf)
   n <- length(amostra)
-  a <- 4.5
   -n * log(theta) + theta * sum(log(amostra - a))
 }
 
@@ -21,7 +21,6 @@ theta_hat <- coef(mle_est)
 
 # Calcular quartil para p = 0.25
 p <- 0.25
-a <- 4.5
 quartil_25 <- a * (1 - p)^(-1 / theta_hat)
 
 # Calcular valor verdadeiro do quartil para theta = 3.4
